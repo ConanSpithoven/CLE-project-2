04d0c308569b9d0a9924dd38034a3e197903833c
dollyhaze <0937335@hr.nl>
2016-09-28 21:32:43 +0900

    Add files via upload
    Arduino code

---
+++ wifi_geeetech.ino
@@ -0,0 +1,34 @@
+#include <WiFi.h>
+#include <SPI.h>
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte server[] = { 64, 233, 187, 99 }; // Google
+
+char ssid[] = "kappa123";
+char pass[] = "0987654321";
+int keyIndex = 0;
+int status = WL_IDLE_STATUS;
+
+
+IPAddress ip;
+
+void setup() {
+
+  
+  if (WiFi.status() == WL_NO_SHIELD) {
+    delay(5000);
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+  
+  Serial.println("shield found");
+  
+ 
+
+  delay(2000);
+}
+
+void loop() {
+
+}
